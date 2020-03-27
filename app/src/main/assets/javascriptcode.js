09daa1d98c63ed27f77f542a4abda55abca2f2c1
Elber Abimael Alves Ribeiro <elberyo@gmail.com>
2020-03-27 17:43:29 -0300

    feat: Icones e correções de javascript

--- app/src/main/assets/javascriptcode.js
+++ app/src/main/assets/javascriptcode.js
@@ -1,21 +1,20 @@
 window.androidObj = function AndroidClass(){};
 
+//Variaveis e funcoes
 var CaixaTexto = document.createElement("p");
 var TextoNativo = document.createTextNode("");
 CaixaTexto.appendChild(TextoNativo);
-
 var inputContainer = document.createElement("p");
 var input = document.createElement("INPUT");
 input.setAttribute("type", "text");
 inputContainer.appendChild(input);
-
 var buttonContainer = document.createElement("p");
 var button = document.createElement("button");
 button.innerHTML = "Enviar para o Android";
 button.style.width = "200px";
 button.style.height = "60px";
 button.addEventListener ("click", function() {
-  window.androidObj.textToAndroid(input.value);
+  window.androidObj.textoparaandroid(input.value);
 });
 buttonContainer.appendChild(button);
 
@@ -23,7 +22,7 @@ document.body.appendChild(CaixaTexto);
 document.body.appendChild(inputContainer);
 document.body.appendChild(buttonContainer);
 
+//Funcao para atualizar o texto recebido
 function updateFromAndroid(message){
     TextoNativo.nodeValue = message;
-    document.getElementById('CaixaTexto').value = message;
 }
--- app/src/main/java/teste/capgemini/MainActivity.kt
+++ app/src/main/java/teste/capgemini/MainActivity.kt
@@ -1,49 +1,45 @@
 package teste.capgemini
 
-import android.content.pm.ApplicationInfo
-import android.os.Build
 import android.os.Bundle
-import android.text.Editable
 import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    @RequiresApi(Build.VERSION_CODES.KITKAT)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        my_web_view.settings.javaScriptEnabled = true
-        my_web_view.addJavascriptInterface(JavaScriptInterface(), JAVASCRIPT_OBJ)
-        my_web_view.webViewClient = object : WebViewClient() {
+        minha_webview.settings.javaScriptEnabled = true
+        minha_webview.addJavascriptInterface(JavaScriptInterface(), JAVASCRIPT)
+        minha_webview.webViewClient = object : WebViewClient() {
             override fun onPageFinished(view: WebView, url: String) {
                 if (url == BASE_URL) {
                     injectJavaScriptFunction()
                 }
             }
         }
-        my_web_view.loadUrl(BASE_URL)
+        minha_webview.loadUrl(BASE_URL)
 
         btOkNativo.setOnClickListener {
-            my_web_view.evaluateJavascript("javascript: " +
-                    "updateFromAndroid(\"" + editTextNativo.text + "\")", null)
+                minha_webview.evaluateJavascript("javascript: " +
+                        "updateFromAndroid(\"" + editTextNativo.text + "\")", null)
         }
     }
 
     override fun onDestroy() {
-        my_web_view.removeJavascriptInterface(JAVASCRIPT_OBJ)
+        minha_webview.removeJavascriptInterface(JAVASCRIPT)
         super.onDestroy()
     }
 
     private fun injectJavaScriptFunction() {
-        my_web_view.loadUrl("javascript: " +
-                "window.androidObj.textToAndroid = function(message) { " +
-                JAVASCRIPT_OBJ + ".textFromWeb(message) }")
+        minha_webview.loadUrl("javascript: " +
+                "window.androidObj.textoparaandroid = function(message) { " +
+                JAVASCRIPT + ".textFromWeb(message) }")
     }
 
 
@@ -55,7 +51,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        private val JAVASCRIPT_OBJ = "javascript_obj"
+        private val JAVASCRIPT = "JAVASCRIPT"
         private val BASE_URL = "file:///android_asset/webview.html"
     }
 }
